db5a2c3666e2329917b50bab3d88c1180545bb30
Faruk <penpos.app@gmail.com>
2026-09-09 11:29:17 +0300

    deploy: otomatik guncelleme

--- backend/src/app.js
+++ backend/src/app.js
@@ -103,8 +103,8 @@ export const createServer = () => {
     app.use(cors(opts))
     app.options('*', cors(opts))
   }
-  app.use(express.json({ limit: '10mb' }))
-  app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }))
   app.use('/public', express.static(path.join(__dirname, '..', 'public')))
   const uploadsStaticOptions = {
     setHeaders: (res, filePath) => {
